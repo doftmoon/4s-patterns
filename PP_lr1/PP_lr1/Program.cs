f93e77d033d1d08930832b5b094f467b91f5813f
DoftMoon <DoftMoon@users.noreply.github.com>
2024-02-10 10:49:21 +0300

    update

---
+++ PP_lr1/PP_lr1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace PP_lr1
+{
+	internal class Program
+	{
+		public class Test
+		{
+			private const int _AGE = 19;
+			public const int AGE = 20;
+			protected const int TEM = 21;
+
+
+			public Test() { }
+			public Test(int age) { }
+
+			private void _Meke()
+			{
+
+			}
+			public void Meke()
+			{
+
+			}
+			protected void Mekee()
+			{
+
+			}
+		}
+
+		interface IFoo
+		{
+			void Foo();
+		}
+
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
